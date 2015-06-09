50db1b10e304b4e899ae6a57be85ddf06b06c1a8
zach powers <zcharlop@rockefeller.edu>
2015-06-09 15:34:46 -0400

    make all taxonomy processing specific by case

--- mibig_tools/process_cluster.py
+++ mibig_tools/process_cluster.py
@@ -31,6 +31,7 @@ def general_cluster_data(data):
     return results
 
 
+
 def process_taxonomy(tax):
     """
     Needs to Handle the Taxonomy from NCBI
@@ -43,100 +44,16 @@ def process_taxonomy(tax):
     except:
         taxonomy = "Unknown" * 7
 
-    #Canonical Taxonomy
+    # Handle By Taxonomy Length
     ######################################################################
     ######################################################################
-    if len(taxonomy) == 7:
-        #classic texonomy
-        k,p,c,o,f,g,s =  taxonomy
-
-    elif len(taxonomy) == 12:
-        # extended taxonomy
-        k,p,c,o,f,subfam,tribe,subtribe,g,subgenus,s,subspecies = taxonomy
-
-    # Case-By-Case: Longer
-    ######################################################################
-    ######################################################################
-    elif len(taxonomy) == 8:
-        tax1 = ['Bacteria', 'Actinobacteria', 'Actinobacteridae', 'Actinomycetales', 'Streptomycineae', 'Streptomycetaceae', 'Streptomyces', 'Streptomyces albidoflavus group']
-        tax2 = ['Bacteria', 'Actinobacteria', 'Actinobacteridae', 'Actinomycetales', 'Corynebacterineae', 'Mycobacteriaceae', 'Mycobacterium', 'Mycobacterium avium complex (MAC)']
-        if taxonomy == tax1 or tax2:
-            # extended taxonomy
-            k,p,c,o,suborder,f,g,subgenus = taxonomy
-            s = "Unknown"
-
-        else:
-            print len(taxonomy)
-            print taxonomy
-
-    elif len(taxonomy) == 9:
-        tax1 = ['Eukaryota', 'Fungi', 'Dikarya', 'Ascomycota', 'Pezizomycotina', 'Leotiomycetes', 'Helotiales', 'Sclerotiniaceae', 'Botrytis']
-        tax2 = ['Eukaryota', 'Fungi', 'Dikarya', 'Ascomycota', 'Pezizomycotina', 'Leotiomycetes', 'Helotiales', 'Helotiaceae', 'Glarea']
-        if taxonomy == tax1 or tax2:
-            # extended taxonomy
-            superkingdom,k,subkindgdom,p,subphylum,c,o,f,g = taxonomy
-            s = "Unknown"
-
-        else:
-            print len(taxonomy)
-            print taxonomy
-
-    elif len(taxonomy) == 10:
-        tax1 = ['Eukaryota', 'Fungi', 'Dikarya', 'Ascomycota', 'Pezizomycotina', 'Eurotiomycetes', 'Eurotiomycetidae', 'Eurotiales', 'Aspergillaceae', 'Aspergillus']
-        tax2 = ['Eukaryota', 'Fungi', 'Dikarya', 'Ascomycota', 'Pezizomycotina', 'Eurotiomycetes', 'Eurotiomycetidae', 'Eurotiales', 'Aspergillaceae', 'Monascus']
-        if taxonomy == tax1 or tax2:
-            # extended taxonomy
-            superkingdom,k,subkingdom,p,subphlum,c,subclass,o,f,g = taxonomy
-            s = "Unknown"
-        else:
-            print len(taxonomy)
-            print taxonomy
-
-    elif len(taxonomy) == 11:
-        tax1 = ['Eukaryota', 'Fungi', 'Dikarya', 'Ascomycota', 'Pezizomycotina', 'Dothideomycetes', 'Pleosporomycetidae', 'Pleosporales', 'Pleosporineae', 'Pleosporaceae', 'Alternaria']
-        tax2 = ['Eukaryota', 'Fungi', 'Dikarya', 'Ascomycota', 'Pezizomycotina', 'Sordariomycetes', 'Hypocreomycetidae', 'Hypocreales', 'Nectriaceae', 'Fusarium', 'Fusarium fujikuroi species complex']
-        if taxonomy == tax1 or tax2:
-            # extended taxonomy doth=??
-            superkingdom,k,subkingdom,p,subphylum,c,subclass,o,suborder,f,g = taxonomy
-            s = "Unknown"
-        else:
-            print len(taxonomy)
-            print taxonomy
-
-    elif len(taxonomy) == 14:
-        tax1 = ['Eukaryota', 'Viridiplantae', 'Streptophyta', 'Embryophyta', 'Tracheophyta', 'Spermatophyta', 'Magnoliophyta', 'Liliopsida', 'Poales', 'Poaceae', 'BEP clade', 'Ehrhartoideae', 'Oryzeae', 'Oryza']
-        tax2 = ['Eukaryota', 'Viridiplantae', 'Streptophyta', 'Embryophyta', 'Tracheophyta', 'Spermatophyta', 'Magnoliophyta', 'Liliopsida', 'Poales', 'Poaceae', 'PACMAD clade', 'Panicoideae', 'Andropogoneae', 'Sorghum']
-        if taxonomy == tax1 or tax2:
-            # extended taxonomy doth=??
-            superkingdom,k,p,__,___,____,____,c,o,f,x,subfamily,tribe,g = taxonomy
-            s = "Unknown"
-        else:
-            print len(taxonomy)
-            print taxonomy
-
-    elif len(taxonomy) == 16:
-        tax1 = ['Eukaryota', 'Viridiplantae', 'Streptophyta', 'Embryophyta', 'Tracheophyta', 'Spermatophyta', 'Magnoliophyta', 'eudicotyledons', 'Gunneridae', 'Pentapetalae', 'rosids', 'malvids', 'Brassicales', 'Brassicaceae', 'Camelineae', 'Arabidopsis']
-        if taxonomy == tax1:
-            # extended taxonomy doth=??
-            superkingdom,k,p,__,___,____,____,x,xx,xxx,subclass,xxxx,o,f,tribe,g = taxonomy
-            c,s = ["Unknown","Unknown"]
-
-        else:
-            print len(taxonomy)
-            print taxonomy
-
-
-    # Case-By-Case: Shorter
-    ######################################################################
-    ######################################################################
-
-    elif len(taxonomy) == 2:
+    if len(taxonomy) == 2:
         tax1 =  ['Bacteria', 'environmental samples']
         tax2 =  ['Bacteria', 'Unknown']
         if taxonomy == tax1 or tax2:
             # extended taxonomy
             k,p = taxonomy
-            c,o,f,g,s = [p for i in range(5)]
+            c,o,f,g = [p for i in range(4)]
 
         else:
             print len(taxonomy)
@@ -149,16 +66,13 @@ def process_taxonomy(tax):
         if taxonomy == tax1 or tax3:
             # extended taxonomy
             k,p,o,g = taxonomy
-            c,f,s = ["Unknown","Unknown", "Unknown"]
+            c,f = ["Unknown","Unknown"]
         elif taxonomy == tax2:
             k,p,c,g = taxonomy
-            o,f,s = ["Unknown","Unknown", "Unknown"]
-
-
+            o,f= ["Unknown","Unknown"]
         else:
             print len(taxonomy)
             print taxonomy
-
 
     elif len(taxonomy) == 5:
         tax1 = ['Bacteria', 'Cyanobacteria', 'Oscillatoriophycideae', 'Oscillatoriales', 'Kamptonema']
@@ -166,7 +80,7 @@ def process_taxonomy(tax):
         if taxonomy == tax1 or tax2:
             # extended taxonomy
             k,p,c,o,f = taxonomy
-            g, s = ["Unknown","Unknown"]
+            g ="Unknown"
 
         else:
             print len(taxonomy)
@@ -178,15 +92,98 @@ def process_taxonomy(tax):
         if taxonomy == tax1 or tax2:
             # extended taxonomy
             k,p,c,o,f,g = taxonomy
-            s = "Unknown"
 
         else:
             print len(taxonomy)
             print taxonomy
 
-    # Print Errors: You Shouldn't Get Here
-    ######################################################################
-    ######################################################################
+    elif len(taxonomy) == 7:
+        #classic taxonomy
+        tax1 = ['Bacteria', 'Actinobacteria', 'Actinobacteridae', 'Actinomycetales', 'Micromonosporineae', 'Micromonosporaceae', 'Verrucosispora']
+        tax2 = ['Bacteria', 'Actinobacteria', 'Actinobacteridae', 'Actinomycetales', 'Pseudonocardineae', 'Pseudonocardiaceae', 'Kutzneria']
+        if taxonomy == tax1 or tax2:
+            k,p,subclass,o,suborder,f,g =  taxonomy
+            c="Actinobacteria"
+
+        else:
+            print len(taxonomy)
+            print taxonomy
+
+    elif len(taxonomy) == 8:
+        tax1 = ['Bacteria', 'Actinobacteria', 'Actinobacteridae', 'Actinomycetales', 'Streptomycineae', 'Streptomycetaceae', 'Streptomyces', 'Streptomyces albidoflavus group']
+        tax2 = ['Bacteria', 'Actinobacteria', 'Actinobacteridae', 'Actinomycetales', 'Corynebacterineae', 'Mycobacteriaceae', 'Mycobacterium', 'Mycobacterium avium complex (MAC)']
+        if taxonomy == tax1 or tax2:
+            # extended taxonomy
+            k,p,c,o,suborder,f,g,subgenus = taxonomy
+
+        else:
+            print len(taxonomy)
+            print taxonomy
+
+    elif len(taxonomy) == 9:
+        tax1 = ['Eukaryota', 'Fungi', 'Dikarya', 'Ascomycota', 'Pezizomycotina', 'Leotiomycetes', 'Helotiales', 'Sclerotiniaceae', 'Botrytis']
+        tax2 = ['Eukaryota', 'Fungi', 'Dikarya', 'Ascomycota', 'Pezizomycotina', 'Leotiomycetes', 'Helotiales', 'Helotiaceae', 'Glarea']
+        if taxonomy == tax1 or tax2:
+            # extended taxonomy
+            superkingdom,k,subkindgdom,p,subphylum,c,o,f,g = taxonomy
+
+        else:
+            print len(taxonomy)
+            print taxonomy
+
+    elif len(taxonomy) == 10:
+        tax1 = ['Eukaryota', 'Fungi', 'Dikarya', 'Ascomycota', 'Pezizomycotina', 'Eurotiomycetes', 'Eurotiomycetidae', 'Eurotiales', 'Aspergillaceae', 'Aspergillus']
+        tax2 = ['Eukaryota', 'Fungi', 'Dikarya', 'Ascomycota', 'Pezizomycotina', 'Eurotiomycetes', 'Eurotiomycetidae', 'Eurotiales', 'Aspergillaceae', 'Monascus']
+        if taxonomy == tax1 or tax2:
+            # extended taxonomy
+            superkingdom,k,subkingdom,p,subphlum,c,subclass,o,f,g = taxonomy
+        else:
+            print len(taxonomy)
+            print taxonomy
+
+    elif len(taxonomy) == 11:
+        tax1 = ['Eukaryota', 'Fungi', 'Dikarya', 'Ascomycota', 'Pezizomycotina', 'Dothideomycetes', 'Pleosporomycetidae', 'Pleosporales', 'Pleosporineae', 'Pleosporaceae', 'Alternaria']
+        tax2 = ['Eukaryota', 'Fungi', 'Dikarya', 'Ascomycota', 'Pezizomycotina', 'Sordariomycetes', 'Hypocreomycetidae', 'Hypocreales', 'Nectriaceae', 'Fusarium', 'Fusarium fujikuroi species complex']
+        if taxonomy == tax1 or tax2:
+            # extended taxonomy doth=??
+            superkingdom,k,subkingdom,p,subphylum,c,subclass,o,suborder,f,g = taxonomy
+        else:
+            print len(taxonomy)
+            print taxonomy
+
+    elif len(taxonomy) == 12:
+        tax1 = ['Eukaryota', 'Fungi', 'Dikarya', 'Ascomycota', 'Pezizomycotina', 'Dothideomycetes', 'Pleosporomycetidae', 'Pleosporales', 'Pleosporineae', 'Pleosporaceae', 'Alternaria', 'Alternaria alternata group']
+        tax2 = ['Eukaryota', 'Fungi', 'Dikarya', 'Ascomycota', 'Pezizomycotina', 'Dothideomycetes', 'Pleosporomycetidae', 'Pleosporales', 'Pleosporineae', 'Didymellaceae', 'mitosporic Didymellaceae', 'Phoma']
+        tax3 = ['Eukaryota', 'Fungi', 'Dikarya', 'Ascomycota', 'Pezizomycotina', 'Dothideomycetes', 'Pleosporomycetidae', 'Pleosporales', 'Pleosporineae', 'Leptosphaeriaceae', 'Leptosphaeria', 'Leptosphaeria maculans complex']
+        if taxonomy == tax1 or tax3:
+            superkingdom,k,subkingdom,p,subphylum,c,subclass,o,suborder,f,g,subgenus = taxonomy
+        elif taxonomy == tax2:
+            superkingdom,k,subkingdom,p,subphylum,c,subclass,o,suborder,f,_,g = taxonomy
+
+        else:
+            print len(taxonomy)
+            print taxonomy
+
+    elif len(taxonomy) == 14:
+        tax1 = ['Eukaryota', 'Viridiplantae', 'Streptophyta', 'Embryophyta', 'Tracheophyta', 'Spermatophyta', 'Magnoliophyta', 'Liliopsida', 'Poales', 'Poaceae', 'BEP clade', 'Ehrhartoideae', 'Oryzeae', 'Oryza']
+        tax2 = ['Eukaryota', 'Viridiplantae', 'Streptophyta', 'Embryophyta', 'Tracheophyta', 'Spermatophyta', 'Magnoliophyta', 'Liliopsida', 'Poales', 'Poaceae', 'PACMAD clade', 'Panicoideae', 'Andropogoneae', 'Sorghum']
+        if taxonomy == tax1 or tax2:
+            # extended taxonomy doth=??
+            superkingdom,k,p,__,___,____,____,c,o,f,x,subfamily,tribe,g = taxonomy
+        else:
+            print len(taxonomy)
+            print taxonomy
+
+    elif len(taxonomy) == 16:
+        tax1 = ['Eukaryota', 'Viridiplantae', 'Streptophyta', 'Embryophyta', 'Tracheophyta', 'Spermatophyta', 'Magnoliophyta', 'eudicotyledons', 'Gunneridae', 'Pentapetalae', 'rosids', 'malvids', 'Brassicales', 'Brassicaceae', 'Camelineae', 'Arabidopsis']
+        if taxonomy == tax1:
+            # extended taxonomy doth=??
+            superkingdom,k,p,__,___,____,____,x,xx,xxx,subclass,xxxx,o,f,tribe,g = taxonomy
+            c="Unknown"
+
+        else:
+            print len(taxonomy)
+            print taxonomy
 
     else:
         print len(taxonomy)
@@ -208,5 +205,4 @@ def process_taxonomy(tax):
             "class": c,
             "order": o,
             "family": f,
-            "genus": g,
-            "species": s}
+            "genus": g}
